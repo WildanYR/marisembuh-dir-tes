a5f9a6d71194cbd6061644885f57a2b570b4cb8f
WildanYR <wildanyahya99@gmail.com>
2021-10-28 07:15:23 +0700

    add loader

---
+++ azzakiyyah/loader.donasi.app.js
@@ -0,0 +1,12 @@
+var headTagElement = document.getElementsByTagName('head')[0];
+
+const modulePreload = document.createElement('link');
+modulePreload.rel = 'modulepreload';
+modulePreload.href = '/vendor.donasi.app.js';
+
+const styleLink = document.createElement('link');
+styleLink.rel = 'stylesheet';
+styleLink.href = 'https://cdn.jsdelivr.net/gh/WildanYR/marisembuh-dir-tes@azm-1.0/azzakiyyah/donasi.app.css';
+
+headTagElement.appendChild(modulePreload);
+headTagElement.appendChild(styleLink);
